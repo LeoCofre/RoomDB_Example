634f7d6aa7068aa94d4907ed3e7664a501826b37
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-07-26 20:22:12 -0400

    Agregamos nav_graph  y fragmentos.

--- app/src/main/java/cl/awakelab/roomdb_example/MainActivity.kt
+++ app/src/main/java/cl/awakelab/roomdb_example/MainActivity.kt
@@ -3,6 +3,19 @@ package cl.awakelab.roomdb_example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ [ ] Navigation
+ [ ] Fragmento agregar
+ [ ] Crear Interfaz
+ [ ] Room
+      [ ] _Dao
+      [ ] _Base Datos
+      [ ]_Entity
+ [ ] Fragmento listado
+ [ ] RecyclerView
+
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
